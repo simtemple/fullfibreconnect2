b2006346ec9f527e9426d1efd3415bc59a976f19
Simon Ndulagwa <stndulagwa@gmail.com>
2024-09-22 00:35:28 +0100

    Update api.php

--- api.php
+++ api.php
@@ -3,4 +3,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Add user authentication here!
 
-echo json_encode(\Icuk\BroadbandAvailabilityPhp\BroadbandAvailabilityProxy::handle_cli_or_postcode("ExampleAPIUsername", "ExampleAPIPassword123"));
+Get https://api-proxy.ofcom.org.uk/broadband/coverage/{PostCode}
+
+
