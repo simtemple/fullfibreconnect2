6bdd1da7c59afa9604c5eccc5981904a54b35fbe
Simon Ndulagwa <stndulagwa@gmail.com>
2024-09-22 01:00:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Broadband Availability Example</title>
+  <title>Broadband Availability </title>
   <?php
     require __DIR__ . '/vendor/autoload.php';
     $ba = new \fullfibreconnect\BroadbandAvailabilityPhp\BroadbandAvailability("api.php");
